a03492a7ae526ccfe6c12fd67b005b3b51f50bfc
Rafael Klein <rafael.augustoklein04@gmail.com>
2023-11-14 13:02:47 -0300

    alterações nas interações e logos

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,25 @@ function ativaLetra(elemento) {
     arrTexto.forEach((letra, i)=> {
         setTimeout (()=>{
             elemento.innerHTML += letra;
-        }, 150 * i)
+        }, 110 * i)
     })
 }
 const titulo = document.querySelector('.digitando')
 ativaLetra(titulo);
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Aguarde 500 milissegundos (ou o tempo desejado) antes de adicionar a classe
+    setTimeout(function() {
+      var apresentacaoEscritaHome = document.querySelector('.apresentacaoEscritaHome');
+      apresentacaoEscritaHome.classList.add('slide-in');
+    }, 500); // Tempo em milissegundos
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Aguarde 500 milissegundos (ou o tempo desejado) antes de adicionar a classe
+    setTimeout(function() {
+      var apresentacaoEscritaHome = document.querySelector('.linksContainer');
+      apresentacaoEscritaHome.classList.add('slide-in');
+    }, 500); // Tempo em milissegundos
+  });
+  
